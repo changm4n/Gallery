fa16ec624afa6b068af60db0e328ae8b3aaccc14
lcm6528 <lcmini6528@gmail.com>
2020-02-12 22:54:52 +0900

    Unit Test 추가

--- GalleryTests/GalleryTests.swift
+++ GalleryTests/GalleryTests.swift
@@ -7,28 +7,60 @@
 //
 
 import XCTest
+import Alamofire
+import RxSwift
+
 @testable import Gallery
 
 class GalleryTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    let disposeBag = DisposeBag()
+    let sampleHTML = """
+<div class="item-wrapper">
+  <img class="jq-lazy" data-src="https://www.gettyimagesgallery.com/wp-content/uploads/2019/07/GettyImages-1158180907.jpg" alt="Gstaad Town Centre from Slim Aarons Snow fine art photography" />
+  <div class="text-wrapper mt-auto">
+    <h5 class="image-title">Gstaad Town Centre</h5>
+    <button class="btn btn-primary btn-enquire" data-id="6728" >Enquire</button>
+  </div>
+</div>
+"""
+    let TIMEOUT: TimeInterval = 5
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testGetHTML() {
+        let promise = expectation(description: "Completion handler invoked")
+        
+        GalleryService.shared.getHTML().subscribe(onNext: { (html) in
+            promise.fulfill()
+        }, onError: { (error) in
+            XCTAssertThrowsError("error")
+        }).disposed(by: disposeBag)
+        
+        waitForExpectations(timeout: TIMEOUT, handler: nil)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testParsing() {
+        let promise = expectation(description: "Completion handler invoked")
+        
+        GalleryService.shared.parse(html: sampleHTML).subscribe(onNext: { (photos) in
+            XCTAssertNotNil(photos.first)
+            promise.fulfill()
+        }, onError: { (error) in
+            XCTAssertThrowsError("parsing error")
+        }).disposed(by: disposeBag)
+        
+        waitForExpectations(timeout: TIMEOUT, handler: nil)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testGetImage() {
+        let promise = expectation(description: "Completion handler invoked")
+        
+        let url = URL(string: "https://www.gettyimagesgallery.com/wp-content/uploads/2019/07/GettyImages-1158180907.jpg")
+        GalleryService.shared.getImage(byURL: url).subscribe(onNext: { (image) in
+            promise.fulfill()
+        }, onError: { (error) in
+            XCTAssertThrowsError("get image error")
+        }).disposed(by: disposeBag)
+        
+        waitForExpectations(timeout: TIMEOUT, handler: nil)
     }
-
 }
